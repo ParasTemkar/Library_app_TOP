b0b2d6a464828b1092bb1e4cd82e8ea7ae38d89f
ParasTemkar <parastemkar0204@gmail.com>
2025-03-19 22:09:44 +0530

    Make book adding modal functional.

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
+// library storage and book constructor.
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.id=crypto.randomUUIDC();
-    this.added= Date.now();
+// to make date readable. got from web.
+const formattedDate = new Date(Date.now()).toISOString().slice(0, 19).replace("T", " ");
+console.log(formattedDate);
+
+function Book(title, author, pages, read,genre) {
+    this.id=crypto.randomUUID();
+    this.added= formattedDate;
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.genre = genre;
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
-}
-
+// Opening and closing of book form.
 const openAddBook = document.querySelector('#openAddBook');
 const addBook = document.querySelector('#addBook');
 const closeAddBook = document.querySelector('#closeAddBook');
 
 openAddBook.addEventListener('click', ()=> addBook.showModal());
 closeAddBook.addEventListener('click', ()=> addBook.close());
+
+// To accept form data and add into Library.
+document.getElementById("book-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("input[name='title']").value;
+  const author = document.querySelector("input[name='author']").value;
+  const pages = document.querySelector("input[name='pages']").value;
+  const read = document.querySelector("input[name='read']").checked; 
+  const genre = document.querySelector("select[name='genre']").value;
+
+  let newBook = new Book(title, author, pages, read, genre);
+  myLibrary.push(newBook);
+
+  console.log(myLibrary);
+});
 
